7a7b3d5837bc4c42df192622bf8ba7caed627821
yuriy.drubinskiy <ydrubins@yahoo.com>
2025-01-29 12:21:34 -0600

    1/29 update

--- Unit 0 - Fundementals/0.2 - Functions.py
+++ Unit 0 - Fundementals/0.2 - Functions.py
@@ -1,19 +1,33 @@
-#Declaring a function
-pass
+# Declaring a function
+
+
+# def greeting():
+#     print("Hello there")
+#
+# greeting()
 
 ###########################
 
 #Declaring a function with a parameter
-pass
+# def greeting2(a_name):
+#     print(f'Hello {a_name}')
+#
+#
+# name = input("Enter your name: ")
+# greeting2(name)
 
 ###########################
 
 
 #Declaring a function with multiple parameters
-pass
-
-#An error occurs if we call the function without the same number of parameters it expects
-pass
+# def greeting3(first_name : str, last_name : str, age : int):
+#     print(f'Hello {first_name} {last_name}, you are {age} yars old')
+#
+# greeting3('Yuriy', 'Drubinskiy', '38')
+# greeting3('38', 'Y', 'D')
+# greeting3('Bob', 'Dole', 80)
+#
+# greeting3('John') # Gives an error, not enough arguments
 
 ###########################
 
@@ -21,12 +35,14 @@ pass
 #Positional vs. keyword parameters
 # Keyword parameters do not have to be passed in
 #... They take the default value when not passed in
-pass
+# def greeting4(first_name = 'Marquise', last_name = 'Xavier'):
+#     print(f'Hello {first_name} {last_name}')
 
-pass
-pass
-pass
-pass
+# greeting4()
+# greeting4("yuriy")
+# greeting4(last_name = 'Drubinskiy')
+# greeting4(first_name = 'Clint', last_name = "Eastwood")
+
 
 ###########################
 
@@ -34,16 +50,48 @@ pass
 #A function, by default returns the data type NONE
 # In order to have it return back any other data, we need to use the 'return command'
 # This is good practice and often prefered over print()
-pass
 
+def greeting5(first_name, last_name):
+    # print('Function is being executed')
+    return f'{first_name} {last_name}'
+#
 # print('Function called')
-pass #Does not show anything
-pass #Print the return from the function
-
-pass #save output from function to a variable
-pass
+# greeting5('Yuriy', 'Drubinskiy') #Does not show anything
+# hello = greeting5('Yuriy', 'Drubinskiy')
+#
+# print(hello) #save output from function to a variable
 
 ###########################
 
 #Functions can call other functions
-pass
+def message(a,b):
+    full_name = greeting5(a,b)
+    return f'Hello {full_name} + enjoy your stay'
+
+print(message('Yuriy', "Drubinskiy"))
+
+
+
+### PRACTICE ###
+"""Write a function called is_float that accepts a string and returns a boolean indicating 
+    whether the string is a valid float value. 
+    
+Not valid examples:
+1) 1.1.1
+2) 1.2a
+3) a.1
+4) 100
+
+Valid Example:
+4) 1.0
+5) 1.1
+
+"""
+
+# Strategy:
+    # 1) Look for exactly one decimal place
+    # 2) Check that each character is an integer
+        # Note: Since '.' is not an integer, we can remove it when we check for an integer (still need to keep the original value though)
+
+def is_float(info):
+    pass
