8cb5b43b2d9ca77eb9e123f8d3ffe69d5623632f
yuriy.drubinskiy <ydrubins@yahoo.com>
2025-01-29 11:26:25 -0600

    initial commit

---
+++ Unit 0 - Fundementals/Exercises/Exercise_0.1b_List_Comprehension.py
@@ -0,0 +1,51 @@
+"""
+Exercise 0.1b - List Comprehension
+
+    - Remember to use List Comprehension to solve these -
+"""
+
+"""
+Problem 1 - 
+
+Given the list of names, create and print a new list with only the names that have more then three letters.
+"""
+names = ['Tom', 'Bob', 'James', 'Frank', 'Ron', 'Jon', 'Greg']
+
+#YOUR SOLUTION HERE
+
+
+"""
+Problem 2 -
+
+Given the list of numbers as strings, create and print a new list with 10 added to every number in the list.
+
+    - NOTE: It is possible and SHOULD be done with one line using a single list comprehension
+    
+"""
+numbers = ['28', '11', '56', '32', '13']
+
+#YOUR SOLUTION HERE
+
+
+
+"""
+Challenge (OPTIONAL - This one is more difficult)
+
+Given the numbers from above, create and print a new list that contain only the PRIME numbers.
+
+HINT - You might want to make a function that checks for a prime and is called from within your list comprehension.
+
+      - Take a look at the example is_even function to see how this could be structured. 
+"""
+
+#EXAMPLE of calling a function from a list comprehension to check if a number is even
+n = [2,3,4,5]
+def is_even(n):
+    if n%2 == 0:
+        return n
+
+evens = [i for i in n if is_even(i)]
+print(evens)
+
+
+#YOUR SOLUTION HERE
---
+++ Unit 0 - Fundementals/Exercises/Exercise_0.2_Functions.py
@@ -0,0 +1,22 @@
+"""
+The Collatz number is a famous unsolved problem in math that can be easily explained with three simple rules.
+
+From a starting number, n, follow three rules to get the next number in the sequence:
+1)	If n is even, the next number n is n / 2.
+2)	If n is odd, the next number n is n * 3 + 1.
+3)	If n is 1, stop. Otherwise, repeat.
+
+Example: Start with n = 26. If you follow the rules, the sequence of number would be:
+    	26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1
+
+Your task:
+    1)	Ask the user to enter a starting integer greater than 0
+
+    2)	Write a function called collatz_sequence that can return a list representing the sequence
+        that follows the Collatz rules for n.
+
+     - 	For example by typing in collatz_sequence(number), where number = 100, you should get:
+            [100, 50, 25, 76, 38, 19, 58, 29, 88, 44, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1]
+
+"""
+##YOUR SOLUTION HERE
