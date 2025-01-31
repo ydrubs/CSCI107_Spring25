d4449f41da2ca9bc31b2f10a79fd43a2479b1d1f
yuriy.drubinskiy <ydrubins@yahoo.com>
2025-01-31 10:14:28 -0600

    initial commit

---
+++ Unit 0 - Fundementals/0.2b - Functions_in_class_practice.py
@@ -0,0 +1,30 @@
+"""
+Write a function that accepts a string representing a phone number as an input and validates if the phone number is valid.
+If the phone number is valid the code returns True otherwise it returns False
+
+A valid phone number will be of the form: 555-555-5555
+
+    All of the following are NOT valid:
+        1) 5-555-555-5555
+        2) 5a5-555-5555
+        3) 555-5555-555
+        4) 555 - 555 - 5555
+
+HINT: You can use the command <string>.split('<split_character>')
+to seperate string <string> into a list using a <split_character> as a seperator
+
+"""
+
+
+def valid_phone_number(number: str):
+    pass
+
+
+phone_number = '555-555-5555'
+valid = valid_phone_number(phone_number)
+print(valid)
+
+
+
+
+
