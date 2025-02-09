75c9330f62722f050109464f0dc73df9c698a0c2
yuriy.drubinskiy <ydrubins@yahoo.com>
2025-02-09 15:19:27 -0600

    initial commit

---
+++ Unit 0 - Fundementals/Exercises/Exercise_0.4_Exception_Handling.py
@@ -0,0 +1,11 @@
+"""
+Exercise 0.4
+
+Rewrite the  Twilzzelrs script from the  beginning of this lesson using what you learned about exception handling.
+
+Implement the following:
+    1) A ValueError when someone tries to add a negative amount to their cart with a custom message
+    2) An error saying something like ‘invalid input’ when a non-interger value is entered.
+    3) A generic exception for any other errors saying something “like an unknown error occurred”
+    4) Add one to the twizzler_count variable if it is a valid input
+"""
