8ced20f661d4a99672a8323ae9fc9546f866bf65
yuriy.drubinskiy <ydrubins@yahoo.com>
2025-02-10 10:35:08 -0600

    Changed wording for req. #4

--- Unit 0 - Fundementals/Exercises/Exercise_0.4_Exception_Handling.py
+++ Unit 0 - Fundementals/Exercises/Exercise_0.4_Exception_Handling.py
@@ -7,5 +7,5 @@ Implement the following:
     1) A ValueError when someone tries to add a negative amount to their cart with a custom message
     2) An error saying something like ‘invalid input’ when a non-interger value is entered.
     3) A generic exception for any other errors saying something “like an unknown error occurred”
-    4) Add one to the twizzler_count variable if it is a valid input
+    4) Add that number to the twizzler_count variable if it is a valid input
 """
