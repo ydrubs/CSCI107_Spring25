d8ca5c8277506f1fd5a0d8bededed2358c5dea82
ydrubs <ydrubins@yahoo.com>
2025-02-27 13:18:02 -0600

    initial commit

---
+++ Unit 1 - Object Oriented Programming/2_Developing Classes and Methods/dealership.py
@@ -0,0 +1,43 @@
+"""
+This is the code that implements the Car class from the modules:
+    1) vehicle 3
+    2) vehicle 4
+    3) vehicle 5
+    4) vehicle 5 inheritance
+"""
+
+# from vehicle3 import Car
+
+
+
+
+
+
+
+#######################################
+# from vehicle4 import Car
+
+
+
+
+
+
+
+#######################################
+##Getter / Setter
+
+
+
+
+
+
+
+###########################################
+# from vehicle5 import Car
+
+
+
+
+
+###########################################
+# from vehicle5_inheritence import GasolineCar, ElectricCar
---
+++ Unit 1 - Object Oriented Programming/2_Developing Classes and Methods/vehicle3.py
@@ -0,0 +1,6 @@
+"""
+In this program we add a few attributes to the class.
+
+We develop the methods that allow our car class to keep track of the test-drive information for  dealership.
+"""
+
