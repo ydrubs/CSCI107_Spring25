6d337691df2fee44910421a293f5a088258e5229
ydrubs <ydrubins@yahoo.com>
2025-03-03 08:12:48 -0600

    initial commit

---
+++ Unit 1 - Object Oriented Programming/2_Developing Classes and Methods/vehicle4.py
@@ -0,0 +1,6 @@
+"""
+In this program we explore how to use private methods to hide information.
+
+We also look at how to use getter and setter method to access and change information in a safer manner.
+"""
+
