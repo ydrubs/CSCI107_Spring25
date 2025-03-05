d8cd5699401a6655b814970cc3466a8cd97897ea
ydrubs <ydrubins@yahoo.com>
2025-03-05 10:14:43 -0600

    3/3 update

--- Unit 1 - Object Oriented Programming/2_Developing Classes and Methods/dealership.py
+++ Unit 1 - Object Oriented Programming/2_Developing Classes and Methods/dealership.py
@@ -6,28 +6,48 @@ This is the code that implements the Car class from the modules:
     4) vehicle 5 inheritance
 """
 
-from vehicle3 import Car
-car1 = Car('Honda', 'Accord', 'blue', '20000')
-
-m1 = car1.increase_mileage(10)
-print(m1)
-m2 = car1.increase_mileage(8)
-print(m2)
-
-car1.mileage = 0 # Reset miles back to 0
-
-drive_recipet = car1.test_drive('A123', 12)
-print(drive_recipet)
-
-
+# from vehicle3 import Car
+# car1 = Car('Honda', 'Accord', 'blue', '20000')
+#
+# m1 = car1.increase_mileage(10)
+# print(m1)
+# m2 = car1.increase_mileage(8)
+# print(m2)
+#
+# car1.mileage = 0 # Reset miles back to 0
+#
+# drive_recipet = car1.test_drive('A123', 12)
+# print(drive_recipet)
+#
+#
 
 
 
 
 #######################################
-# from vehicle4 import Car
+from vehicle4 import Car
+car1 = Car('Toyota', 'Camry', 'Purple', 37000, 29000)
+# print(car1.__price)
 
+# print(car1.__price)
 
+car1.__price = 900 # Creates a new attribute DOES NOT change the __price
+print(car1.__price)
+print(dir(car1))
+
+print(car1._Car__price)
+print(car1._Car__dealer_price)
+
+car1._Car__price = 20000
+print(car1._Car__price)
+
+p = car1.get_price()
+print("here", p)
+
+car1.set_price(30000)
+print(car1.get_price())
+
+car1.set_price(28999)
 
 
 
--- Unit 1 - Object Oriented Programming/2_Developing Classes and Methods/vehicle4.py
+++ Unit 1 - Object Oriented Programming/2_Developing Classes and Methods/vehicle4.py
@@ -3,4 +3,64 @@ In this program we explore how to use private methods to hide information.
 
 We also look at how to use getter and setter method to access and change information in a safer manner.
 """
+class Car:
+    def __init__(self, make, model, color, price, dealer_price, sold = False, ):
+        self.make = make
+        self.model = model
+        self.color = color
+        self.vin = ''
+        self.__price = price
+        self.mpg = 0
+        self.year = 0
+        self.mileage = 0
+        self.sold = sold
+        self.on_lot = False
+        self.numb_of_test_drives = 0
+        self.__dealer_price = dealer_price
+
+
+        self.greeting()
+
+
+    def greeting(self):
+        print( "Welcome to Shady Acres Car Dealership")
+
+    def give_discount(self, percentage):
+        pass
+
+    def increase_mileage(self, miles):
+        self.mileage +=miles
+        return self.mileage
+
+    def sell_car(self, final_price):
+        pass
+
+    def test_drive(self, license_number, amount_driven):
+        print(f'Your plate number is {license_number}')
+        self.numb_of_test_drives +=1
+        self.increase_mileage(amount_driven)
+        return (f'Yay you drove the {self.make} {self.model} '
+                f'it now has {self.mileage} miles on it.')
+
+    def get_price(self):
+        """See the price the car sells for"""
+        print('Thanks for your inquiry, the price is:')
+        return self.__price
+
+    def set_price(self, suggested_price):
+        if suggested_price > self.__dealer_price:
+            print(f"Price was set to {suggested_price}.")
+            self.__price = suggested_price
+        else:
+            print("Get out, you are ripping me off!!")
+
+
+    def get_dealer_price(self):
+        """See the dealers price the car sells for"""
+        return self.__dealer_price
+
+
+if __name__ == '__main__':
+    print('here')
+    car1 = Car('Honda', 'Accord', 'blue', '20000')
 
