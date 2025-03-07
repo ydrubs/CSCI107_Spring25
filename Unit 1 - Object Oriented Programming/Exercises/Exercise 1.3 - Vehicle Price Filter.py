568e4f52be2a3775bcfcc3fdf9e2a93b3cb18927
ydrubs <ydrubins@yahoo.com>
2025-03-07 12:07:40 -0600

    initial commit

---
+++ Unit 1 - Object Oriented Programming/Exercises/Exercise 1.3 - Vehicle Price Filter.py
@@ -0,0 +1,17 @@
+"""
+Copy and paste the vehicle 5 code here.
+
+Expand the vehicle 5 code to implement another class method that returns a list of all cars (their make and model) that are below the price passes in as a parameter.
+
+You can call the method  ‘max_price_search’ or something else
+
+Test your code to validate it works under the if __name__ == '__main__' statement
+
+    NOTE: See Canvas and/or Powerpoint for the test code and example output
+"""
+#Copy and paste the vehicle 5 code here, then expand it
+
+
+if __name__ == '__main__':
+    #Test your code here:
+    pass
