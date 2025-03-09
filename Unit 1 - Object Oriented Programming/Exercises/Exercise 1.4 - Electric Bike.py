09ba6e4c9b3807af9d31466547d182ad08c9f7f4
ydrubs <ydrubins@yahoo.com>
2025-03-09 17:16:17 -0500

    initial commit

---
+++ Unit 1 - Object Oriented Programming/Exercises/Exercise 1.4 - Electric Bike.py
@@ -0,0 +1,23 @@
+"""
+Exercise 1.4 Electric Bike
+
+Copy the vehicle5 inheritence code here as a starting point.
+
+Add an electric bike class, call it 'Ebike'. The class should inherit from the electric vehicle class.
+
+The Ebike class should add a new attribute called 'battery_cap_percent' passed in when the instance is created.
+This attribute keeps track of the battery capacity which effects the miles_per_charge.
+
+Add a method called get miles per charge to the Ebike class that returns the miles per_charge as a percentage of battery capacity.
+For example: battery_cap_percent passed in is 90, and miles_per_charge passed in when the Ebike instance is created is set to 120,
+then get_miles_per_charge would return 108 (because 108 is 9 percent of 120)
+
+Test your class to ensure it works.
+"""
+
+if __name__=='__main__':
+    pass
+    # parameters to pass in to the Ebike blass: make, model, color, price, dealer_price, miles_per_charge, battery_cap_percent
+    # ebike = Ebike('Scooty-puff', 'Junior', 'yellow', 5000, 2500, 120, 90)
+
+
