f2dbad06fea234b7e936fe8012b000453ef1fd72
ydrubs <ydrubins@yahoo.com>
2025-03-10 11:03:11 -0500

    initial commit

---
+++ Unit 1 - Object Oriented Programming/2_Developing Classes and Methods/vehicle5_inheritence.py
@@ -0,0 +1,16 @@
+"""
+Demonstration of how inheritence works in OOP with Python
+
+We create two new classes, ElectricCar and GasolineCar that inherit from the Car class.
+We then write a method (give_discount) that gives a discount on an electric vehicle purchase.
+"""
+
+class Car():
+    pass
+
+
+
+if __name__=='__main__':
+    pass
+    # my_car = GasolineCar('Toyota', 'Corolla', 'red', 30500, 26000, 35)
+    # my_car2 = ElectricCar('Toyota', 'Prius', 'red', 30500, 26000, 55)
