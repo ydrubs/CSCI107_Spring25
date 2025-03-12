6c16f38eb25ea46b448dc0d2c706769b15a0f76c
ydrubs <ydrubins@yahoo.com>
2025-03-12 10:34:02 -0500

    initial commit

---
+++ Unit 1 - Object Oriented Programming/Exercises/Exercise 1.5 -  Magic Method with rational numbers.py
@@ -0,0 +1,17 @@
+"""
+Copy and Paste the RationalNumber Class that we developed in class
+
+Use the RationalNumber class to implement multiplication and division of rational numbers using the magic methods __mul__ and __trudiv__
+
+Use the __add__ and __sub__ methods as your examples. REMEMBER to account for if the other operand is not part of the class
+
+SHOW that your operations work by utilizing them with your created objects.
+
+"""
+pass
+
+
+
+
+if __name__ == '__main__':
+    pass
