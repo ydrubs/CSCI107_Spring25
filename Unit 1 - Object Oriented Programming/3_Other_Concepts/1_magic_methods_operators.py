b274de38ac842eceb70a73cf67a04c6247a71d41
ydrubs <ydrubins@yahoo.com>
2025-03-12 10:27:40 -0500

    initial commit

---
+++ Unit 1 - Object Oriented Programming/3_Other_Concepts/1_magic_methods_operators.py
@@ -0,0 +1,43 @@
+"""
+Exploring Magic methods
+"""
+
+"""
+part 1 - given a class called WordFind, we want to:
+    1) create an instance from a word passed in to the __init__ of the class
+    2) Return the string passed in when calling the object through print
+    3) Index into the object as though it were a normal string 
+"""
+pass
+
+
+
+
+
+
+
+
+
+"""
+part 2 - How does Python utilize operator overload with arithmetic functions?
+    Given an arithmetic operation, how does Python hande operations such as '+' or '*' on different data types 
+
+    NOTICE - Same operations could be used across different data types 
+"""
+pass
+
+
+
+
+
+
+
+
+"""
+Part 3 - Implementing Custom Operators using magic methods
+
+    Create a new class to make working with Rational Numbers (Fractions) easier. The objects in the class should - 
+        - be able to utilize the 4 basic operations '+' '-' '*' and '/' in order to calculate and return another rational number (fraction) 
+"""
+pass
+
