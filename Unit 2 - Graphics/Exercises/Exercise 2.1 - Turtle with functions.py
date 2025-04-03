c6ae7284aea247c706ac41488f4b05c705480586
ydrubs <ydrubins@yahoo.com>
2025-04-03 15:27:46 -0500

    initial commit

---
+++ Unit 2 - Graphics/Exercises/Exercise 2.1 - Turtle with functions.py
@@ -0,0 +1,7 @@
+"""
+Exercise 2.1 Turtle with Classes
+
+Write a function called draw_design that accepts the starting position of the turtle (pencil)
+and a color parameters, then draws an interesting design on the screen when called.
+
+"""
