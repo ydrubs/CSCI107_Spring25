226df84ab1de2118727ea822e8af4c281a8afd95
ydrubs <ydrubins@yahoo.com>
2025-04-04 10:21:52 -0500

    initial commit

--- Unit 2 - Graphics/Exercises/Exercise 2.1 - Turtle with functions.py
+++ Unit 2 - Graphics/Exercises/Exercise 2.1 - Turtle with functions.py
@@ -4,4 +4,6 @@ Exercise 2.1 Turtle with Classes
 Write a function called draw_design that accepts the starting position of the turtle (pencil)
 and a color parameters, then draws an interesting design on the screen when called.
 
+See an example of this in Canvas by copy/pasting the code and running it.
+
 """
