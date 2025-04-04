065c0f407999b25bf1e4435f58e9eaa178b2e555
ydrubs <ydrubins@yahoo.com>
2025-04-04 12:40:02 -0500

    4/4 update

--- Unit 2 - Graphics/L1_turtle_basics.py
+++ Unit 2 - Graphics/L1_turtle_basics.py
@@ -10,35 +10,44 @@ The Turtle module could also be used for elaborate, complex graphics.
 
 import turtle as t
 
-pass #Create the screen object
+t.Screen() #Create the screen object
 
 #Specify the size and location of the screen (second two parameters optional).
-pass #screen width, screen height, horizontal distance from the top left of your computer screen, and vertical distance from the top left of your computer screen
-pass #Set bg color
-pass #set the title of the window
+t.setup(600, 500, 0, 0) #screen width, screen height, horizontal distance from the top left of your computer screen, and vertical distance from the top left of your computer screen
+t.bgcolor('SpringGreen3') #Set bg color
+t.title("Demo for a screen") #set the title of the window
 
-pass #Change cursor to turtle; starts at position (x = 0, y = 0) by default.
+t.shape('turtle') #Change cursor to turtle; starts at position (x = 0, y = 0) by default.
+# t.speed('slowest')
 
 #Screen orientation
-pass #lift pen up
-pass #Upper left corner is negative width/2, postive height/2
-t.write('300,250') #Upper right corner is positive widht/2, postive height/2
-pass #Lower left corner is negative widht/2, positive height/2
-pass #Lower right corner is negative widht/2, negative height/2
+t.up() #lift pen up
+t.goto(-300 + 50, 250 -50) #Upper left corner is negative width/2, postive height/2
+t.write('(300,250)') #Upper right corner is positive widht/2, postive height/2
+t.goto(300 - 50, 250 -50) #Lower left corner is negative widht/2, positive height/2
+t.write('(300,250)') #Lower right corner is negative widht/2, negative height/2
 
-pass #Put pencil at the middle of the screen
-pass # Don't show turtle movement
+t.down() #Put pencil at the middle of the screen
+t.tracer(0) # Don't show turtle movement
 
 
 #Define movements
-pass # place pen down
+# t.pendown() # place pen down
+# t.forward(200)
+# t.right(90)
+# t.backward(50)
+# t.right(30)
+# t.forward(50)
+#
+# for i in range(30):
+#     t.forward(100)
+#     t.left(20)
+#
 
-##Specifiy movements
-pass
 
 
-pass#Change pen size
-pass#Change color
+t.pensize(6) #Change pen size
+t. color('green') #Change color
 
 #More drawing demos
 t.up()
@@ -48,14 +57,22 @@ t.goto(300,250)
 t.up()
 t.goto(-300,250)
 t.down()
-t.goto(300,-250)
-
-t.up()
 t.color('blue')
+t.goto(300,-250)
+#
+# t.up()
+# t.color('blue')
 
 
 ##Draw with a loop
+t.goto(0,0)
+t.up()
+for i in range(12):
+    t.goto(-300 + 50*i, 0)
+    t.down()
+    t.goto(-300+50*i+35, 0)
+    t.up()
 
 
-pass #redraws the screen (usually used after calling a change within a program so won't see effect here)
-pass #Keep window open after drawing finishes
+t.update() #redraws the screen (usually used after calling a change within a program so won't see effect here)
+t.done() #Keep window open after drawing finishes
