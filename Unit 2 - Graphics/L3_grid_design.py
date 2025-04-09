84af94df6d50013cec57faa3fec51e269c063453
ydrubs <ydrubins@yahoo.com>
2025-04-09 10:56:07 -0500

    tweaked formatting of template

--- Unit 2 - Graphics/L3_grid_design.py
+++ Unit 2 - Graphics/L3_grid_design.py
@@ -4,7 +4,7 @@ First with specific numbers, then by generalizing the dimensions.
 Grids are useful in many different GUI applications such as games, menu layouts, organizers, etc..
 """
 
-# import turtle as t
+import turtle as t
 
 #Set up screen
 
@@ -20,11 +20,14 @@ Grids are useful in many different GUI applications such as games, menu layouts,
 #Draw the Horizontal lines
 
 
+
 """place a dot in the top left cell"""
 pass #Move pen up
+
 # To find the center of the first cell:
-    #1) -350 is the left most point of the grid, we need to go halfway across this cell
-    #2) 250 is the top most point of the grid, we need to go halfway down this cell.
+    #1) -350 is the left most point of the grid, we need to go halfway across this cell which has a lenght of 100
+    #2) 250 is the top most point of the grid, we need to go halfway down this cell, again lenght=100.
+
 pass
 pass# Since each cell is 100x100, we can make that the diameter of the dot
 
@@ -60,20 +63,31 @@ t.done()
 
 
 
-##Exercise 3.2
-"""Write a function that accepts row, column, and color as a parameter.
+##Exercise 2.2
+"""Write a function called place_dot that accepts row, column, and color as a parameter.
 
-The function should display a dot in the middle of the row, column specified with the color passed in. 
-The dot should be the same diamater as the cell side legnth.
-The  dot should draw correctly even if the number of rows, columns, and/or cell dimensions is changed.
+    1) The function should display a dot in the middle of the row, column specified with the color passed in. 
+    2) The dot should be the same diamater as the cell side legnth.
+    3) The  dot should draw correctly even if the number of rows, columns, and/or cell dimensions is changed.
+        So it should be defined relative to the grid and not in absolute terms
+    4) Finally, if the row and/or columsn passed into the function exceeds the rows/columns the grid generates, the message 'out of range'
+        should be printed in the terminal.
+        
+"""
 
-So it should be defined relative to the grid and not in absolute terms
-
-Finally, if the row and/or columns passed into the function exceeds the rows/columns the grid generates, the message 'out of range'
-should be printed in the terminal."""
-
-def place_dot(row:int, column:int, color = 'black'):
+def place_dot(*args): #Replace *args with specific parameters
     pass
-
-
-#Run functions
+#
+#
+if __name__ == '__main__':
+#     #Run functions
+#     screen_setup()
+#     vertical_lines()
+#     horizontal_lines()
+#
+#     #Use the function to draw dots in the grid below
+#     pass
+#     pass
+#     pass
+#
+    t.done()
