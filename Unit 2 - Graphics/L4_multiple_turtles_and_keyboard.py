daf125a1d053497419a55d1c91e59cfed67f2407
ydrubs <ydrubins@yahoo.com>
2025-04-11 10:22:23 -0500

    initial commit

---
+++ Unit 2 - Graphics/L4_multiple_turtles_and_keyboard.py
@@ -0,0 +1,62 @@
+"""In this lesson we will explore how to use multiple turtles for drawing on to the screen.
+We create instance variables from the turtle class rather then using the generic class as in previous lessons.
+"""
+import turtle
+
+## Create an instance of the turtle screen object
+
+
+
+## Create an array to store multiple turtle objects
+
+
+
+
+
+##Create and store turtles into the list
+
+# turtle 1
+
+
+
+# turtle 2
+
+
+
+# turtle 3
+
+
+## Send commands to each turtle individuals
+
+
+
+
+
+##Send commands to each turtle by looping through the list
+
+
+
+
+# Write to the screen but with formatting
+
+##Define functions for keyboard presses
+# When up key is pressed trigger this function
+
+
+
+# When down key is pressed trigger this function
+
+
+# When left key is pressed trigger this function
+
+
+
+# When right key is pressed trigger this function
+
+
+
+##Create a keypress listener and assosiate with a key
+# Note that these methods come from the turtle class itself rather then instances of individuak turtles
+
+
+turtle.done()
