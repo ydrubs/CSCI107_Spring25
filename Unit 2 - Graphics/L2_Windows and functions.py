38caffe8a5c0373d0b5a3d8a2677e3ef9ed1595c
ydrubs <ydrubins@yahoo.com>
2025-04-11 12:31:07 -0500

    updated code

--- Unit 2 - Graphics/L2_Windows and functions.py
+++ Unit 2 - Graphics/L2_Windows and functions.py
@@ -1,46 +1,89 @@
 """In this lesson you will learn how to automate design tasks effectively by laying out windows with variables
 and creating functions"""
 
-import pass
+import turtle as t
 
 ##Let's start by defining a few variables for our window.
 ##This will help generalize the dimensions when screen size changes
-pass
+screen_width = 800
+screen_height = 600
 
+max_X = screen_width//2
+min_X = -(screen_width//2)
+
+max_Y = screen_height//2
+min_Y = -(screen_height//2)
 
 
 
 ##Function to setup the screen
-pass
-
+def screen_setup():
+    t.Screen()
+    t.setup(screen_width, screen_height)
+    t.bgcolor('cyan')
+    t.title('Turtle function practice')
 
 
 
 # Write the dimensions of the screen on the screen
-pass
+def screen_dimension_write():
+    t.up()
+    t.goto(min_X +10, min_Y+10)
+    loc = str(min_X) + ',' + str(min_Y)
+    t.write(loc)
+
+    t.up()
+    t.goto(max_X -100, min_Y+10)
+    loc = str(max_X) + ',' + str(min_Y)
+    t.write(loc)
+
+    t.up()
+    t.goto(max_X -100, max_Y-20)
+    loc = str(max_X) + ',' + str(max_Y)
+    t.write(loc)
+
+    t.up()
+    t.goto(min_X +10, max_Y-20)
+    loc = str(min_X) + ',' + str(max_Y)
+    t.write(loc)
+
 
 
 
 ## Function to draw the border.
+def draw_border(offset : int, color='black', size = 5):
     """
-    This function draws a border around the screen 
-    
-    Args:
-        offset (int): The offset of the border-lines from the ege of the screen
-        color (str): The color of the border-lines 
-    
-    Returns:
-        None
+        This function draws a border around the screen
+
+        Args:
+            offset (int): The offset of the border-lines from the ege of the screen
+            color (str): The color of the border-lines
+            size (int): Changes the size of the border
+
+        Returns:
+            None
     """
-pass
+    t.up()
+    t.pensize(size)
+    t.color(color)
+
+    t.goto(max_X-offset, max_Y-offset)
+    t.down()
+
+    t.goto(max_X - offset, min_Y + offset)
+    t.goto(min_X + offset, min_Y + offset)
+    t.goto(min_X + offset, max_Y - offset)
+    t.goto(max_X - offset, max_Y - offset)
+
 
 
 ###Use our functions
-pass #Setup the screen
-pass #Write the dimensions of the screen to the screen
-pass # Draw a border in red
-pass# Draw a border in green
-pass # Draw a border in purple
+screen_setup() #Setup the screen
+screen_dimension_write() #Write the dimensions of the screen to the screen
+
+draw_border(50) # Draw a border in black
+draw_border(100, 'green')# Draw a border in green
+draw_border(10, 'purple', 1) # Draw a border in purple
 
 
 t.done()
--- Unit 2 - Graphics/L3_grid_design.py
+++ Unit 2 - Graphics/L3_grid_design.py
@@ -4,63 +4,106 @@ First with specific numbers, then by generalizing the dimensions.
 Grids are useful in many different GUI applications such as games, menu layouts, organizers, etc..
 """
 
-import turtle as t
+# import turtle as t
+#
+# #Set up screen
+# t.Screen()
+# t.setup(800, 600)
+# t.hideturtle()
+# t.tracer(False)
+# t.bgcolor('lightgreen')
+# t.pensize(5)
+#
+#
+# #We will create a 5(rows)x7(columns) grid where each cell is 100x100 and there is a border of 50 pixel around each edge.
+# #It could help sketching and labeling the dimensions on a paper to layout the logic
+#
+# #Draw the vertical lines
+# t.up()
+# for i in range(-350, 351, 100):
+#     t.goto(i, -250)
+#     t.down()
+#     t.goto(i, 250)
+#     t.up()
+#
+#
+#
+# #Draw the Horizontal lines
+# t.up()
+# for i in range(-250, 251, 100):
+#     t.goto(-350, i)
+#     t.down()
+#     t.goto(350, i)
+#     t.up()
+#
+#
+# """place a dot in the top left cell"""
+# t.up() #Move pen up
+#
+# # To find the center of the first cell:
+#     #1) -350 is the left most point of the grid, we need to go halfway across this cell which has a lenght of 100
+#     #2) 250 is the top most point of the grid, we need to go halfway down this cell, again lenght=100.
+#
+# t.goto(-350 +50, 250-50)
+# t.dot(2, 'black')# Since each cell is 100x100, we can make that the diameter of the dot
+#
+#
+# t.done()
 
-#Set up screen
 
 
 
-#We will create a 5(rows)x7(columns) grid where each cell is 100x100 and there is a border of 50 around each edge.
-#It could help sketching and labeling the dimensions on a paper to layout the logic
-#Draw the vertical lines
-
-
-
-
-#Draw the Horizontal lines
-
-
-
-"""place a dot in the top left cell"""
-pass #Move pen up
-
-# To find the center of the first cell:
-    #1) -350 is the left most point of the grid, we need to go halfway across this cell which has a lenght of 100
-    #2) 250 is the top most point of the grid, we need to go halfway down this cell, again lenght=100.
-
-pass
-pass# Since each cell is 100x100, we can make that the diameter of the dot
-
-
-t.done()
 
 
 
 ###Let's rewrite this program to allow us to create a grid based on the rows, columns, and column size desired
-# import turtle as t
+import turtle as t
 
 #Set grid variables to fully define custom grid and window
-
+columns = 10
+rows = 12
+cell_dim = 40
+border = 40
 
 
 ##Define window and edges based on variables
-
+screen_width = columns * cell_dim + 2*border
+screen_height = rows * cell_dim + 2*border
 
 #Corners/edges
-
+max_X = screen_width//2
+min_X = -max_X
+max_Y = screen_height//2
+min_Y = -max_Y
 
 #Set up screen function
-
-
+def screen_setup(size = 5, bgcolor = 'lightgreen'):
+    t.Screen()
+    t.setup(screen_width, screen_height)
+    t.tracer(False)
+    t.bgcolor(bgcolor)
+    t.pensize(size)
 
 
 #Draw vertical lines  based on variables
-
+def vertical_lines():
+    t.up()
+    for i in range(min_X + border, max_X-border+1, cell_dim):
+        t.goto(i, min_Y+border)
+        t.down()
+        t.goto(i, max_Y-border)
+        t.up()
 
 
 
 #Draw horizonal lines
-
+def horizontal_lines():
+    t.up()
+    for i in range(min_Y+border, max_Y-border+1, cell_dim):
+        t.goto(min_X+border, i)
+        t.down()
+        t.goto(max_X-border, i)
+        t.up()
 
 
 ##Exercise 2.2
@@ -81,13 +124,14 @@ def place_dot(*args): #Replace *args with specific parameters
 #
 if __name__ == '__main__':
 #     #Run functions
-#     screen_setup()
-#     vertical_lines()
-#     horizontal_lines()
+    screen_setup()
+    vertical_lines()
+    horizontal_lines()
 #
 #     #Use the function to draw dots in the grid below
 #     pass
 #     pass
 #     pass
 #
+
     t.done()
