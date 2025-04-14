2ae21a59d4fa3407148276e8a65e5626d9112158
ydrubs <ydrubins@yahoo.com>
2025-04-14 10:14:05 -0500

    initial commit

---
+++ Unit 2 - Graphics/L5_turtle_classes.py
@@ -0,0 +1,77 @@
+import turtle
+import random
+
+
+##This class creates and draws individua rectangles
+##It recieves the rect collection parameter in order to allow us to add rectangles to the list from the RectCollection class
+# #We also pass turtle.Turtle into the class in order to allow us to use turtle methods on the objects created with the class
+        #...otherwise we would not be abe to, unless we define turtle in the class itself
+
+class
+
+        pass#initialize turtle
+        pass#Allows the size of the rectangle to be individually changed
+        pass#Don't draw until ready
+        pass#set the rectangle objects position
+
+        pass#call the add method of the rectangle collection object in order to add the instance to a list
+
+
+    ##method to draw each rectangle
+    pass
+
+"""
+    Use trig for some extra funzies
+        Make sure to add self.x and self.y as atttributes of each objects position 
+"""
+
+
+
+##This is the builder class that manages the set of objects created in the above class
+##Pass width and height of the screen as parameters
+pass
+
+        pass#create the screen
+        pass#Setup screen dimensions
+        pass#Set the BG dcolor
+
+        pass#List to hold the rectangles
+
+
+    ##Method that adds the rectangle to the list
+    pass
+
+
+
+    ##Method that changes the size of the rectangles when called
+        ##Iterate through:
+        #...1) Randomly resize each rectangle
+        #...2) Call the draw method for each rectangle
+        #...3) Update the screen after each call to draw.
+    pass
+        pass
+             # Change the size of each rectangle object
+             # Debug info
+             # Call the draw method in the RectBody class for each object
+             # Update the screen
+
+
+
+###Create objects and test
+if __name__ == '__main__':
+    # Create a screen object from the RectCollection class and individual objects from the rect_body class
+    pass
+
+    ##Iterate by calling each object in the list and using its draw method
+    pass
+
+
+    #Call the update method from the RectCollection class
+    pass
+
+
+    ##Show our rectangle object list in raw form
+    pass
+
+
+    turtle.done()#Update the screen
