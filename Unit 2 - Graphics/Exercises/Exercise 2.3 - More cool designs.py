5ff207680fda5c67ca9dbe5a3905c584289aeec4
ydrubs <ydrubins@yahoo.com>
2025-04-14 10:17:49 -0500

    initial commit

---
+++ Unit 2 - Graphics/Exercises/Exercise 2.3 - More cool designs.py
@@ -0,0 +1,9 @@
+"""
+Exercise 2.3
+
+Use the code we wrote in class, L5_turtle_classes as a template to create your own unique design
+
+    - Experiment with the draw method and the update method
+    - Feel free to add your own methods, attributes, and even classes for extra fun and challenge
+
+"""
