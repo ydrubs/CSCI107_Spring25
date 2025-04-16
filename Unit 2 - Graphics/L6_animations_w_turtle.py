055b58cdd077777d06c08d531c1008c4391887bf
ydrubs <ydrubins@yahoo.com>
2025-04-16 11:14:47 -0500

    initial commit

---
+++ Unit 2 - Graphics/L6_animations_w_turtle.py
@@ -0,0 +1,58 @@
+"""
+
+In this program we learn to create an animation using the Turtle library.
+
+We have two classes.
+    One class creates the screen and manages the animation by looping through each object that is animated
+    The other class controls the animation of each individual object
+
+A while loop ensures that the animation loops forever
+
+"""
+
+import turtle
+import random
+
+# Class representing a snowflake
+pass
+    pass
+        pass # Initialize the Turtle class
+        pass  # Snowflakes are represented as squares
+        pass  # Random floating point size for each snowflake, try other numbers such as (1,3) for fun and profit
+        pass
+        pass
+        pass # Set the initial location of the snowflake
+
+    pass
+        """Place the snowflake at a random position at the top of the screen."""
+        pass # Anywhere across the screen
+        pass # Towards the upper half of the screen
+        pass # Move each snowflake (the square) to the position chosen
+
+    pass
+        """Move the snowflake downward, and reset if it falls out of the screen."""
+        pass # Change how fast it falls
+        pass  # Reset position when the snowflake leaves the screen
+
+
+# Class representing the snowglobe animation
+pass
+    pass
+
+
+        pass  # Disable automatic screen updates for smooth animation, TRY with this not disabled, it looks kind of cool
+
+        pass  # Create 100 snowflakes
+
+    pass
+        """Start the snowglobe animation."""
+        pass
+            pass
+                pass  # Make each snowflake fall
+
+            pass  # Update the screen
+
+# Main program
+if __name__ == "__main__":
+    # Create a snowglobe with specified dimensions
+    # Start the animation
